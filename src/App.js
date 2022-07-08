4bd88761c9387b51fb3ea5b1635627fa2d7fcac3
minh quang <minhquangkid@gmail.com>
2022-07-08 21:58:19 +0700

    xong lab03.2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
 import "./App.css";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES, // DISHES được import ở trên
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,10 +25,10 @@ class App extends Component {
             <NavbarBrand href="/">This is Bootstrap</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
-}
+} // props trong tag Menu là dishes vơi tham số được truyền vào chính là biến state của class App
 
 export default App;
